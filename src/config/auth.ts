c00a6b404b84fd915d8673a549de58c81d4d1479
FranciscoMaik <chiconunes3081@gmail.com>
2020-10-24 09:42:27 -0300

    Add typeorm

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'fb18933c831646a45dcfa00d0de380bf',
+    expiresIn: '1d',
+  },
+};
---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,43 @@
+import { id } from 'date-fns/esm/locale';
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import AppError from '../errors/AppError';
+import authConfig from '../config/auth';
+
+interface Tokenpayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  // validação do token
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('JWT token is missing', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decoded as Tokenpayload;
+
+    request.user = {
+      id: sub,
+    };
+
+    console.log(decoded);
+
+    return next();
+  } catch (error) {
+    throw new AppError('Invalid JWT token', 401);
+  }
+}
